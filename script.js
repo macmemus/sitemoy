06c174966a20197bddb32b66170c73c0ccc807db
macmemus <130259890+macmemus@users.noreply.github.com>
2024-05-29 08:52:26 +0300

    поправки

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function openDrawer() {
 function closeDrawer() {
     document.getElementById("drawer").style.width = "0";
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.querySelector('.search-input');
+    const guides = document.querySelectorAll('.guide-card');
+
+    searchInput.addEventListener('input', function () {
+        const query = searchInput.value.toLowerCase();
+
+        guides.forEach(guide => {
+            const title = guide.querySelector('h2').textContent.toLowerCase();
+            const content = guide.querySelector('p').textContent.toLowerCase();
+
+            if (title.includes(query) || content.includes(query)) {
+                guide.style.display = '';
+            } else {
+                guide.style.display = 'none';
+            }
+        });
+    });
+});
